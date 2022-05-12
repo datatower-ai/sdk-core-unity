d2caf799d7f4394af246e4e4a61a3fa7d8fc5e59
xiaosailing <xiaosailing@nodetower.com>
2022-05-12 15:07:56 +0800

    delete  wrong method

--- Assets/ROIQuerySDK/ROIQueryCore/ROIQuerySDKAPI.cs
+++ Assets/ROIQuerySDK/ROIQueryCore/ROIQuerySDKAPI.cs
@@ -62,16 +62,5 @@ namespace ROIQuery
 
         #endregion
         
-        private void OnApplicationPause(bool pause)
-        {
-            if (pause)
-            {
-                ROIQueryAnalytics.OnAppBackground();
-            }
-            else
-            {
-                ROIQueryAnalytics.OnAppForeground();
-            }
-        }
     }
 }
