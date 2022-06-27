4a4ed1d6843ecbfc574e1304b7ffbcda8e2b8a63
xiaosailing <xiaosailing@nodetower.com>
2022-06-27 19:20:24 +0800

    update sdk version to 1.2.2

--- Assets/ROIQuerySDK/ROIQueryCore/ROIQuerySDKAPI.cs
+++ Assets/ROIQuerySDK/ROIQueryCore/ROIQuerySDKAPI.cs
@@ -21,9 +21,9 @@ namespace ROIQuery
         /// <summary>
         /// 当前 Unity SDK 版本
         /// </summary>
-        public readonly static string SDK_VERSION = "1.2.0";
+        public readonly static string SDK_VERSION = "1.2.2";
 
-        [Header("ROIQuerySDK-v1.2.0")]
+        [Header("ROIQuerySDK-v1.2.2")]
         
         [Tooltip("由后台分配")] public string androidAppId = "";
         
