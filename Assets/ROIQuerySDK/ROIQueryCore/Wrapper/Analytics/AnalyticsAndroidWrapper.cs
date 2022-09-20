1bdaa923bbe13f02b5f584d88220a7d73b480b9b
shijunxing <shijunxing@nodetower.com>
2022-09-20 14:01:30 +0800

    bugfix 1）sdk_type、sdk_version加上#

--- Assets/ROIQuerySDK/ROIQueryCore/Wrapper/Analytics/AnalyticsAndroidWrapper.cs
+++ Assets/ROIQuerySDK/ROIQueryCore/Wrapper/Analytics/AnalyticsAndroidWrapper.cs
@@ -21,8 +21,8 @@ namespace ROIQuery
 
             //替换sdk版本信息
             Dictionary<string, object> properties = new Dictionary<string, object>();
-            properties.Add("sdk_type", "Unity");
-            properties.Add("sdk_version", sdkVersion);
+            properties.Add("#sdk_type", "Unity");
+            properties.Add("#sdk_version", sdkVersion);
             string jsonStr = R_Utils.Parse2JsonStr(properties);
             AndroidJavaObject jsonObject = R_Utils.Parse2JavaJSONObject(jsonStr);
 
