3bc4de0d7481a1c378959c16f6e2b11058c9ab61
shijunxing <shijunxing@nodetower.com>
2023-02-07 15:56:23 +0800

    屏蔽iOS

--- Assets/DataTower/DTCore/Wrapper/Analytics/AnalyticsIOSWrapper.cs
+++ Assets/DataTower/DTCore/Wrapper/Analytics/AnalyticsIOSWrapper.cs
@@ -1,75 +1,77 @@
-﻿namespace DataTower
+﻿using System.Collections.Generic;
+
+namespace DataTower
 {
     public partial class ROIQueryAnalyticsWrapper
     {
 #if UNITY_IOS && !(UNITY_EDITOR)
-        [DllImport("__Internal")]
-        private static extern void initSDK(string appId, bool isDebug, int logLevel, string properties);
-
-        [DllImport("__Internal")]
-        private static extern void roiTrack(string eventName, string properties);
-
-        [DllImport("__Internal")]
-        private static extern void roiFlush();
-
-        [DllImport("__Internal")]
-        private static extern void trackPageOpen(string properties);
-
-        [DllImport("__Internal")]
-        private static extern void trackPageClose(string properties);
-
-        [DllImport("__Internal")]
-        private static extern void trackAppClose(string properties);
-        
-        [DllImport("__Internal")]
-        private static extern string getDataTowerId();
-
-        [DllImport("__Internal")]
-        private static extern void setAccountId(string id);
-
-        [DllImport("__Internal")]
-        private static extern void setFirebaseAppInstanceId(string id);
-        
-        [DllImport("__Internal")]
-        private static extern void setKochavaId(string id);
-
-        [DllImport("__Internal")]
-        private static extern void setAppsFlyerId(string id);
-
-        [DllImport("__Internal")]
-        private static extern void setUserProperties(string properties);
-        
-        [DllImport("__Internal")]
-        private static extern void userSet(Dictionary<string, object> properties = null);
-        
-        
-        [DllImport("__Internal")]
-        private static extern void userSetOnce(Dictionary<string, object> properties = null);
-        
-        [DllImport("__Internal")]
-        private static extern void userUnset(string[] properties);
-        
-        [DllImport("__Internal")]
-        private static extern void userAppend(Dictionary<string, object> properties = null);
-        
-        [DllImport("__Internal")]
-        private static extern void userAdd(Dictionary<string, object> properties = null);
-        
-        [DllImport("__Internal")]
-        private static extern void userDelete();
-        
-        
-        [DllImport("__Internal")]
-        private static extern void setIasOriginalOrderID(string id);
-
-        [DllImport("__Internal")]
-        private static extern void setAdjustId(string id);
-        
-        [DllImport("__Internal")]
-        private static extern void onAppForeground();
-
-        [DllImport("__Internal")]
-        private static extern void onAppBackground();
+        // [DllImport("__Internal")]
+        // private static extern void initSDK(string appId, bool isDebug, int logLevel, string properties);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void roiTrack(string eventName, string properties);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void roiFlush();
+        //
+        // [DllImport("__Internal")]
+        // private static extern void trackPageOpen(string properties);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void trackPageClose(string properties);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void trackAppClose(string properties);
+        //
+        // [DllImport("__Internal")]
+        // private static extern string getDataTowerId();
+        //
+        // [DllImport("__Internal")]
+        // private static extern void setAccountId(string id);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void setFirebaseAppInstanceId(string id);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void setKochavaId(string id);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void setAppsFlyerId(string id);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void setUserProperties(string properties);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void userSet(Dictionary<string, object> properties = null);
+        //
+        //
+        // [DllImport("__Internal")]
+        // private static extern void userSetOnce(Dictionary<string, object> properties = null);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void userUnset(string[] properties);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void userAppend(Dictionary<string, object> properties = null);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void userAdd(Dictionary<string, object> properties = null);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void userDelete();
+        //
+        //
+        // [DllImport("__Internal")]
+        // private static extern void setIasOriginalOrderID(string id);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void setAdjustId(string id);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void onAppForeground();
+        //
+        // [DllImport("__Internal")]
+        // private static extern void onAppBackground();
 
         private void _init()
         {
@@ -79,7 +81,7 @@
             properties.Add("#sdk_version_name", sdkVersion);
             string jsonStr = R_Utils.Parse2JsonStr(properties);
 
-            initSDK(iOSAppId, isDebug, logLevel, jsonStr);
+            // initSDK(iOSAppId, isDebug, logLevel, jsonStr);
             // ROIQuerySDK.CallStatic("initSDK", currentContext,appId,channel,isDebug,logLevel,jsonObject);
             R_Log.Debug("Editor Log: calling init.");
         }
@@ -87,38 +89,38 @@
 
         private void _track(string eventName, Dictionary<string, object> properties = null)
         {
-            string jsonStr = R_Utils.Parse2JsonStr(properties);
-            roiTrack(eventName, jsonStr);
+            // string jsonStr = R_Utils.Parse2JsonStr(properties);
+            // roiTrack(eventName, jsonStr);
             R_Log.Debug("Editor Log: calling track.");
         }
 
         private void _userSet(Dictionary<string, object> properties)
         {
-            userSet(properties);
+            // userSet(properties);
         }
         private void _userSetOnce(Dictionary<string, object> properties)
         {
-            userSetOnce(properties);
+            // userSetOnce(properties);
         }
         
         private void _userAdd(Dictionary<string, object> properties)
         {
-            userAdd(properties);
+            // userAdd(properties);
         }
         
         private static void _userUnset(List<string> properties)
         {
-         userUnset(properties.ToArray());  
+         // userUnset(properties.ToArray());  
         }
         
         private void _userDelete()
         {
-            userDelete();
+            // userDelete();
         }
         
         private void _userAppend(Dictionary<string, object> properties)
         {
-            userAppend(properties);
+            // userAppend(properties);
         }
 
 
@@ -131,29 +133,29 @@
 
         private void _setAccountId(string accountId)
         {
-            setAccountId(accountId);
+            // setAccountId(accountId);
             R_Log.Debug("Editor Log: calling _setAccountId.");
         }
 
         private void _setFirebaseAppInstanceId(string id)
         {
-            setFirebaseAppInstanceId(id);
+            // setFirebaseAppInstanceId(id);
             R_Log.Debug("Editor Log: calling _setFirebaseAppInstanceId.");
         }
 
         private void _setAppsFlyerId(string id)
         {
-            setAppsFlyerId(id);
+            // setAppsFlyerId(id);
             R_Log.Debug("Editor Log: calling _setApps  FlyerId.");
         }
         private void _setKochavaId(string id)
         {
-            setKochavaId(id);
+            // setKochavaId(id);
             R_Log.Debug("Editor Log: calling _setKochavaId.");
         }
           private void _setAdjustId(string id)
         {
-            setAdjustId(id);
+            // setAdjustId(id);
             R_Log.Debug("Editor Log: calling _setKochavaId.");
         }
 
--- Assets/DataTower/DTCore/Wrapper/utils/AnalyticsUtilsIOSWrapper.cs
+++ Assets/DataTower/DTCore/Wrapper/utils/AnalyticsUtilsIOSWrapper.cs
@@ -3,41 +3,41 @@ namespace DataTower
     public partial class AnalyticsUtilsWrapper
     {
 #if UNITY_IOS && !(UNITY_EDITOR)
-        [DllImport("__Internal")]
-        private static extern void trackTimerStart(string eventName);
-
-        [DllImport("__Internal")]
-        private static extern void trackTimerPause(string eventName);
-
-        [DllImport("__Internal")]
-        private static extern void trackTimerResume(string eventName);
-
-        [DllImport("__Internal")]
-        private static extern void trackTimerEnd(string eventName,string properties = null);
+        // [DllImport("__Internal")]
+        // private static extern void trackTimerStart(string eventName);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void trackTimerPause(string eventName);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void trackTimerResume(string eventName);
+        //
+        // [DllImport("__Internal")]
+        // private static extern void trackTimerEnd(string eventName,string properties = null);
       
         private static void _trackTimerStart(string eventName)
         {
-            trackTimerStart(eventName);
-            R_Log.Debug("Editor Log: calling trackTimerStart.");
+            // trackTimerStart(eventName);
+            // R_Log.Debug("Editor Log: calling trackTimerStart.");
         }
 
         private void _trackTimerPause(string eventName)
         {
-            trackTimerPause(eventName);
-            R_Log.Debug("Editor Log: calling trackTimerPause.");
+            // trackTimerPause(eventName);
+            // R_Log.Debug("Editor Log: calling trackTimerPause.");
         }
 
         private void _trackTimerResume(string eventName)
         {
-            trackTimerResume(eventName);
-            R_Log.Debug("Editor Log: calling trackTimerResume.");
+            // trackTimerResume(eventName);
+            // R_Log.Debug("Editor Log: calling trackTimerResume.");
         }
 
         private void _trackTimerEnd(string eventName, Dictionary<string, object> properties)
         {
-            string jsonStr = R_Utils.Parse2JsonStr(properties);
-            trackTimerEnd(eventName,jsonStr);
-            R_Log.Debug("Editor Log: calling trackTimerEnd.");
+            // string jsonStr = R_Utils.Parse2JsonStr(properties);
+            // trackTimerEnd(eventName,jsonStr);
+            // R_Log.Debug("Editor Log: calling trackTimerEnd.");
         }
 #endif
     }
