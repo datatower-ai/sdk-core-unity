e45d4f63f542ca271c39a383f38dddeeace039b6
linkailong <HoiLungLam@outlook.com>
2024-07-30 12:03:27 +0800

    Separated ReportPurchaseSuccess for Android and iOS (IAP)

--- Assets/DataTower/Core/Api/DTIAPReport.cs
+++ Assets/DataTower/Core/Api/DTIAPReport.cs
@@ -6,17 +6,33 @@ namespace DataTower.Core
     public class DTIAPReport
     {
         /// <summary>
-        ///     购买成功上报
+        ///     购买成功上报 (Android)
         /// </summary>
         /// <param name="order">订单id</param>
         /// <param name="sku">商品id</param>
         /// <param name="price">价格， 如 9.99</param>
         /// <param name="currency">货币，如usd</param>
         /// <param name="properties">自定义属性</param>
-        public static void ReportPurchaseSuccess(string order, string sku, double price, string currency, 
+        public static void ReportPurchaseSuccessAndroid(string order, string sku, double price, string currency, 
             Dictionary<string, object> properties = null)
         {
             DTIAPReportWrapper.Instance.ReportPurchaseSuccess(order, sku, price, currency, properties);
+        }
+        
+        
+        /// <summary>
+        ///     购买成功上报 (iOS)
+        /// </summary>
+        /// <param name="order">订单id</param>
+        /// <param name="sku">商品id</param>
+        /// <param name="price">价格， 如 9.99</param>
+        /// <param name="currency">货币，如usd</param>
+        /// <param name="seq">标识</param>
+        /// <param name="entrance">入口</param>
+        public static void ReportPurchaseSuccessIos(string order, string sku, double price, string currency, 
+            string seq, string entrance = "")
+        {
+            DTIAPReportWrapper.Instance.ReportPurchased(order, sku, price, currency, seq, entrance);
         }
     }
 }
