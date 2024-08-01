746d6ef5c292c2b0f6e1966ef7ab39098d2c18cd
Lilin <lilinll77@gmail.com>
2024-08-01 12:09:41 +0800

    脚本添加对应需要处理的组件

--- Assets/DataTower/Sample2/Entry.cs
+++ Assets/DataTower/Sample2/Entry.cs
@@ -9,23 +9,28 @@ namespace DataTower.Sample2
 {
     public class Entry : MonoBehaviour
     {
-        private string _serverUrl = "";
-        private string _appId = "";
+        private string _serverUrl = "https://test.roiquery.com";
+        private string _appId = "dt_d4f13c5e2a7c804c";
         private bool _isDebug = true;
         private bool _manuallyEnableUpload = false;
-        
+
+        public TMP_InputField inputFieldServerUrl;
+
+        public TMP_InputField inputFieldAppId;
+
+
         // Start is called before the first frame update
         private void Start()
         {
-            var inputFieldServerUrl = GameObject.Find("Canvas/InputFieldServerUrl").GetComponent<TMP_InputField>();
             inputFieldServerUrl.onValueChanged.AddListener(delegate { _serverUrl = inputFieldServerUrl.text; });
-            
-            var inputFieldAppId = GameObject.Find("Canvas/InputFieldAppId").GetComponent<TMP_InputField>();
+            inputFieldServerUrl.text = _serverUrl;
+
             inputFieldAppId.onValueChanged.AddListener(delegate { _appId = inputFieldAppId.text; });
-            
+            inputFieldAppId.text = _appId;
+
             var toggleIsDebug = GameObject.Find("Canvas/ToggleIsDebug").GetComponent<Toggle>();
             toggleIsDebug.onValueChanged.AddListener(delegate { _isDebug = toggleIsDebug.isOn; });
-            
+
             var toggleManuallyEnableUpload = GameObject.Find("Canvas/ToggleManuallyEnableUpload").GetComponent<Toggle>();
             toggleManuallyEnableUpload.onValueChanged.AddListener(delegate { _manuallyEnableUpload = toggleManuallyEnableUpload.isOn; });
 
@@ -40,7 +45,11 @@ namespace DataTower.Sample2
 #elif UNITY_IOS && !(UNITY_EDITOR)
                         DTAnalytics.Init("", _appId, _serverUrl, "", DTSDKApi.SDK_VERSION, _isDebug, (int)LogLevel.DEBUG, _manuallyEnableUpload);
                         SceneManager.LoadSceneAsync("DataTower/Sample2/Home");
+#else
+                        DTAnalytics.Init("", _appId, _serverUrl, "", DTSDKApi.SDK_VERSION, _isDebug, (int)LogLevel.DEBUG, _manuallyEnableUpload);
+                        SceneManager.LoadSceneAsync("DataTower/Sample2/Home");
 #endif
+
                     }
                 });
         }
